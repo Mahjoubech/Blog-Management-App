8095fc795ffff215c6a43c36b68408659afb7155
Mahjoubech <charkaouielmahjoub50@gmail.com>
2024-12-22 02:48:45 +0100

    config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#22BAA0',
+                secondary: '#34425A',
+                neutral: '#B0B0B0',
+            }
+        }
+    }
+}
+
 module.exports = {
     content: ["./src/**/*.{php,js}"],
     theme: {
